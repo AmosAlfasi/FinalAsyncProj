f65c6b7f6f63f620efe73ad0e2e3453586f1a559
dianaKra <diana13123@gmail.com>
2022-05-10 20:15:12 +0300

    delete yearly report+add validators

--- services/cost.service.js
+++ services/cost.service.js
@@ -1,8 +1,13 @@
 const Cost = require("../models/cost");
-const userService = require("./user.service");
+const User = require("../models/user");
 
 module.exports = {
-	async insertCost(description, category, year, month, sum) {
+	async insertCost(description, category, year, month, sum, userId) {
+		const user = await User.findOne({ id: userId });
+		if (!user) {
+			console.log(`user with id:${id} not found`);
+			return;
+		}
 		const newCost = await Cost.create({
 			description,
 			category,
